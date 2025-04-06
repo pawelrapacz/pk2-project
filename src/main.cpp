fd0d75b9b3d01ea6e5d6bd60827b0415d5da88e7
Paweł Rapacz <apacz_paw@wp.pl>
2025-04-06 16:04:33 +0200

    Add cli interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,51 @@
 #include <iostream>
+#include "clipper.hpp"
 
-int main() {
-    std::cout << "Hello World!";
+int main(int argc, char** argv) {
+    bool help;
+    std::string coordinates;
+    std::string connectionsTable;
+    std::string traces;
+    std::string outputFile;
+
+
+    CLI::clipper cli;
+    cli.name("MapaMiasta").author("Paweł Rapacz");
+
+    cli.help_flag("--help", "-h")
+        .set(help);
+
+    cli.add_option<std::string>("-coor")
+        .set("file", coordinates)
+        .doc("Input with coordinates")
+        .req();
+
+    cli.add_option<std::string>("-tab")
+        .set("file", connectionsTable)
+        .doc("Input with connections table")
+        .req();
+
+    cli.add_option<std::string>("-q")
+        .set("file", traces)
+        .doc("Input with traces")
+        .req();
+
+    cli.add_option<std::string>("-out")
+        .set("file", outputFile)
+        .doc("Output file")
+        .req();
+
+
+    if (!cli.parse(argc, argv)) {
+        for (auto& i: cli.wrong)
+            std::cout << i << "\n";
+        return 1;
+    }
+
+    if (help)
+        std::cout << cli.make_help();
+
+    // do stuff
+
     return 0;
 }
