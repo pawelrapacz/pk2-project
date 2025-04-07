59464c8d838774286128e35925ec5e5fe8dda6ea
Paweł Rapacz <apacz_paw@wp.pl>
2025-04-07 17:33:02 +0200

    Modify CityGraph to be an UndirectedGraph alias

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "clipper.hpp"
+#include "CityGraph.h"
+#include "UndirectedGraph.h"
 
 int main(int argc, char** argv) {
     bool help;
@@ -36,16 +38,67 @@ int main(int argc, char** argv) {
         .req();
 
 
-    if (!cli.parse(argc, argv)) {
-        for (auto& i: cli.wrong)
-            std::cout << i << "\n";
-        return 1;
-    }
+    // if (!cli.parse(argc, argv)) {
+    //     for (auto& i: cli.wrong)
+    //         std::cout << i << "\n";
+    //     return 1;
+    // }
 
     if (help)
         std::cout << cli.make_help();
 
     // do stuff
+
+    citymap::CityGraph graph;
+    graph.insertVertex("Sklep", {0, 10});
+    graph.insertVertex("Dom", {50, 11});
+    graph.insertVertex("Szpital", {4, 2});
+    graph.insertVertex("Szkoła", {4, 2});
+    graph.addEdge("Szkoła", "Dom");
+    graph.addEdge("Szkoła", "Szpital");
+    graph.addEdge("Szpital", "Sklep");
+
+    std::cout << "Szkoła:\n";
+    for (auto& i : graph.adjacent("Szkoła"))
+        std::cout << i << '\n';
+    
+    std::cout << "\nDom:\n";
+    for (auto& i : graph.adjacent("Dom"))
+        std::cout << i << '\n';
+
+    graph.removeVertex("Dom");
+    std::cout << "\n\nSzkoła:\n";
+    for (auto& i : graph.adjacent("Szkoła"))
+        std::cout << i << '\n';
+
+
+    std::cout << "\n\n#############################\n############################\n\n";
+
+
+    UndirectedGraph<std::string, citymap::Point> g2;
+    g2.size();
+    g2.insertVertex("Sklep", {10, 11});
+    g2.insertVertex("Dom", {10, 15});
+    g2.insertVertex("Szpital", {50, 11});
+    g2.insertVertex("Szkoła", {10, 90});
+    g2.addEdge("Szkoła", "Dom");
+    g2.addEdge("Szkoła", "Szpital");
+    g2.addEdge("Szpital", "Sklep");
+
+    std::cout << "Szkoła:\n";
+    for (auto& i : g2.adjacent("Szkoła"))
+        std::cout << i << '\n';
+    
+    std::cout << "\nDom:\n";
+    for (auto& i : g2.adjacent("Dom"))
+        std::cout << i << '\n';
+
+    g2.removeVertex("Dom");
+    std::cout << "\n\nSzkoła:\n";
+    for (auto& i : g2.adjacent("Szkoła"))
+        std::cout << i << '\n';
+
+    std::cout << g2.getVertexValue("Szpital").x;
 
     return 0;
 }
